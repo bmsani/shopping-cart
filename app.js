cc4dcb14e3fc1f7d71e81d744a4e88215d57697d
bmsani <bmsani6@gmail.com>
2022-02-13 13:10:24 +0600

    price counter added

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-console.log('I am in a separate file')
+function updateProductNumber (product,isIncreasing, price){
+    const productCount = document.getElementById(product + '-number');
+    console.log(productCount);
+    const countNumberString = productCount.value
+    let countNumber = parseInt(countNumberString);
+
+    if(isIncreasing == true){
+        countNumber = countNumber + 1 ;
+    }
+    else if (countNumber > 0){
+        countNumber = countNumber - 1 ;
+    }
+    productCount.value = countNumber;
+
+    const singleProductPrice = document.getElementById(product + '-price');
+    singleProductPrice.innerText = countNumber * price;
+}
+
+document.getElementById('phone-incrise').addEventListener('click', function (){
+    
+    updateProductNumber('phone',true,1219);
+
+})
+
+document.getElementById('phone-decrise').addEventListener('click', function (){
+
+    updateProductNumber('phone',false,1219);
+
+})
+
+document.getElementById('case-incrise').addEventListener('click', function (){
+    
+    updateProductNumber('case',true,59);
+
+})
+
+document.getElementById('case-decrise').addEventListener('click', function (){
+
+    updateProductNumber('case',false,59);
+
+})
+
+
